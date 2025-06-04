e296d93a6f080c8c2bda7ea316bc4737bbd1f645
Jeremy <jeremygdiamond@gmail.com>
2025-06-04 16:26:29 +0200

    checkpoint

--- shell.nix
+++ shell.nix
@@ -1,16 +1,21 @@
-# simple.nix
 with (import <nixpkgs> {});
-  mkShell {
-    buildInputs = [
-      cargo
-      openssl
-      openssl.dev
-    ];
+mkShell {
+  buildInputs = [
+    cargo
+    openssl
+    pkg-config
+  ];
 
-    shellHook = ''
-    find /nix/store -name 'libssl.so*' -or -name 'libcrypto.so*' -exec ls -l {} \; 2>/dev/null
-    echo "set export OPENSSL_DIR"
-    echo "set export OPENSSL_LIB_DIR"
-    echo "set export OPENSSL_INCLUDE_DIR"
-    '';
-  }
+  # These help Rust crates like openssl-sys find the right libs
+  shellHook = ''
+    export OPENSSL_DIR=${openssl.dev}
+    export OPENSSL_LIB_DIR=${openssl.out}/lib
+    export OPENSSL_INCLUDE_DIR=${openssl.dev}/include
+    export OPENSSL_NO_VENDOR=1
+
+    echo "OPENSSL_DIR=$OPENSSL_DIR"
+    echo "OPENSSL_LIB_DIR=$OPENSSL_LIB_DIR"
+    echo "OPENSSL_INCLUDE_DIR=$OPENSSL_INCLUDE_DIR"
+  '';
+}
+
